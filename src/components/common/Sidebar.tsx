b3ab5a2e5d2ee310ee1d7c44776774d98e7ae1ff
vitalii <vilavren@gmail.com>
2023-10-03 19:10:01 +0500

    added: logout

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,3 +1,66 @@
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
+import {
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  Typography,
+} from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { authActions } from '../../store/auth/auth.slice'
+import { AppDispatch, RootState } from '../../store/store'
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>
+  const navigate = useNavigate()
+  const dispatch = useDispatch<AppDispatch>()
+  const user = useSelector((s: RootState) => s.auth.data)
+  const sidebarWidth = 250
+
+  const logout = () => {
+    dispatch(authActions.logout())
+    navigate('/login')
+  }
+
+  return (
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open
+      sx={{
+        width: sidebarWidth,
+        height: '100%',
+        '& > div': { borderRight: 'none' },
+      }}
+    >
+      <List
+        disablePadding
+        sx={{
+          width: sidebarWidth,
+          height: '100vh',
+          backgroundColor: '#292929',
+        }}
+      >
+        <ListItem>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant="body2" fontWeight={700}>
+              {user?.name}
+            </Typography>
+            <IconButton onClick={logout}>
+              <LogoutOutlinedIcon fontSize="small" />
+            </IconButton>
+          </Box>
+        </ListItem>
+      </List>
+    </Drawer>
+  )
 }
