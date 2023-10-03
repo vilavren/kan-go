13fab923ded619bc1d5dc795d17a870a6839e5e7
vitalii <vilavren@gmail.com>
2023-10-03 20:38:40 +0500

    added: create board button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,21 @@
+import LoadingButton from '@mui/lab/LoadingButton'
+import { Box } from '@mui/material'
+
 export const Home = () => {
-  return <div>Home</div>
+  const createBoard = () => {}
+
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <LoadingButton color="info" variant="outlined" onClick={createBoard}>
+        Нажмите чтобы создать первую доску
+      </LoadingButton>
+    </Box>
+  )
 }
