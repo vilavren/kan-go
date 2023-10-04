3b872b2f7479a7ef0129a754b33602bc0378bfcf
vitalii <vilavren@gmail.com>
2023-10-04 17:09:19 +0500

    added: getAll boards

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -8,23 +8,40 @@ import {
   ListItem,
   Typography,
 } from '@mui/material'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
+import { Status } from '../../interfaces/status.enum'
 import { authActions } from '../../store/auth/auth.slice'
+import { getAllBoards } from '../../store/boards/boards.slice'
 import { AppDispatch, RootState } from '../../store/store'
 
 export const Sidebar = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch<AppDispatch>()
   const user = useSelector((s: RootState) => s.auth.data)
+  const { board, boards } = useSelector((s: RootState) => s.boards)
+
+  // const isBoardsLoading = boards.status !== Status.LOADING
+  const boardsItem = boards.items
+  const boardId = board.item?.id
+
   const sidebarWidth = 250
+
+  useEffect(() => {
+    dispatch(getAllBoards())
+    if (boardsItem.length > 0 && boardId === undefined) {
+      navigate(`/boards/${boardsItem[0].id}`)
+    }
+  }, [])
 
   const logout = () => {
     dispatch(authActions.logout())
     navigate('/login')
   }
 
+  console.log(boardsItem)
   return (
     <Drawer
       container={window.document.body}
